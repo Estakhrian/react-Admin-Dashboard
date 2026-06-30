6508de58ca2248ad0e7f6cea85f39d61d6e6201b
Estakhrian <alirezaestakhrian@gmail.com>
2026-07-01 02:35:33 +0430

    add style in dashboard component

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -6,7 +6,7 @@ import ActivityFeed from './ActivityFeed'
 
 const Dashboard = () => {
   return (
-    <div className='space-y-4'>
+    <div className='space-y-4 w-full overflow-hidden'>
       {/**Stats Grid */}
       <StatsGrid />
       {/** chart section */}
